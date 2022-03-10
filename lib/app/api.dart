acdf93852593131e0816b3cda63d50ef376a7eb3
DarienRomero <a20156090@pucp.edu.pe>
2022-03-10 10:24:22 -0500

    previous ios

--- lib/app/api.dart
+++ lib/app/api.dart
@@ -1,3 +1,4 @@
 // const URL_API = 'http://132.148.86.24:8082/reportes/';
-String URL_API = 'https://enlatienda.pe:8443/app_humanidad/';
+// String URL_API = 'https://enlatienda.pe:8443/app_humanidad/';
+String URL_API = 'http://198.12.255.151:8081/api/v0/';
 // String URL_API = '';
